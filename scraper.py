5ae90208920dbc740ce0aed1fb4f81f7953fca1e
EC2 Default User <ec2-user@ip-172-31-2-139.eu-central-1.compute.internal>
2022-12-29 20:16:29 +0000

    try to push data to geno-scraper-s3

--- scraper.py
+++ scraper.py
@@ -2,11 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
 
+# Define the urls for the different genossenschaften
 geno_par = {
     "DWG": "https://www.dwg-online.de/angebote/aktuelle-wohnungsangebote",
-    "BWB": "https://www.bwb-eg.de/wohnungssuche"
+    "BWB": "https://www.bwb-eg.de/wohnungssuche",
+    "WOGEDO": "https://www.wogedo.de/wohnen/aktuelle-angebote/",
+    "SWD": "https://www.swd-duesseldorf.de/service/wohnungsangebot/wohnungssuche/wohnungssuche.html?FORM_SUBMIT=&regionid=0&rooms=0&area=0&flatfilter_submit=Suchen"
 }
 
+# Fetch the html text from the url
 def html_text(url):
     try: 
         html = requests.get(url)
@@ -16,30 +20,63 @@ def html_text(url):
 
     return html.text
 
+def dwg_scraper():    
+    soup = BeautifulSoup(html_text(geno_par['DWG']), 'html.parser')
+
+    # Find all attributes we need
+    list_item = soup.find_all("div", {"class": "immobilie-list-item"})
+    list_image = [x.find_all('a')[0] for x in list_item]
+    url_partial = [x.attrs['href'] for x in list_image]
+
+    # Attach on each url the root-url
+    url = ['https://www.dwg-online.de'+x for x in url_partial]
+
+    return url
 
 
-# def dwg_scraper():    
-#     soup = BeautifulSoup(html_text(geno_par['DWG']), 'html.parser')
+def bwb_scraper():
+    # Find the number of result pages first
+    soup = BeautifulSoup(html_text(geno_par['BWB']), 'html.parser')
+    pagination = soup.find("nav", {"class": "pagination"})
+    page_count = len(pagination.find_all('a')) + 1 # +1 because active page is not scraped
 
-#     # Find all attributes we need
-#     list_item = soup.find_all("div", {"class": "immobilie-list-item"})
-#     list_image = [x.find_all('a')[0] for x in list_item]
-#     url_partial = [x.attrs['href'] for x in list_image]
+    html_text_appended = ''
+    for n in range(1, page_count+1, 1):
+        html_text_appended += html_text(geno_par["BWB"]+f'/page/{n}')
 
-#     # Attach on each url the root-url
-#     url = ['https://www.dwg-online.de'+x for x in url_partial]
+    soup = BeautifulSoup(html_text_appended, 'html.parser')
+    departements = soup.find_all('div', {'class': 'section-wohnung container'})
 
-#     return url
+    departements_expose = [x.find('a', {'class': 'expose-button'}) for x in departements]
+    url = [x.attrs['href'] for x in departements_expose]
+    
+    return url
 
-# Find out how many result pages we have
-soup = BeautifulSoup(html_text(geno_par['BWB']), 'html.parser')
-pagination = soup.find("nav", {"class": "pagination"})
-page_count = len(pagination.find_all('a')) + 1 # +1 because active page is not scraped
 
-html_text_appended = ''
-for n in range(1, page_count+1, 1):
-    html_text_appended += html_text(geno_par["BWB"]+f'/page/{n}')
+def wogedo_scaper():
+    soup = BeautifulSoup(html_text(geno_par['WOGEDO']), 'html.parser')
+    departements = soup.find_all("article", {"class": "wg-offer-articles__items flat wg-bg--white"})
+    url = ["https://www.wogedo.de"+x.find('a', href=True)['href'] for x in departements]
 
-soup = BeautifulSoup(html_text_appended, 'html.parser')
+    return url
 
-print('end')
+
+def swd_scraper():
+    soup = BeautifulSoup(html_text(geno_par['SWD']), 'html.parser')
+    departements = soup.find_all('div', {'class': ['layout_overview block even', 'layout_overview block first even', 'layout_overview block last even', 
+                                                    'layout_overview block odd', 'layout_overview block first odd', 'layout_overview block last odd']})
+    url = ["https://www.swd-duesseldorf.de/"+x.find('a', href=True)['href'] for x in departements]
+
+    return url
+
+if __name__ == "__main__":
+    # pprint(dwg_scraper())
+    # pprint(bwb_scraper())
+    # pprint(wogedo_scaper())
+    # pprint(swd_scraper())
+
+    scraper_functions = [dwg_scraper, bwb_scraper, wogedo_scaper, swd_scraper]
+    for scraper in scraper_functions:
+        pprint(scraper())
+
+    print('run complete')
